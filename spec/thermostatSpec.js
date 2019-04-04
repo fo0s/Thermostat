c18129c7d3d05900a290973c0873c46abe483c0e
Student <student@Makerss-MacBook-Pro.local>
2019-04-04 16:10:53 +0100

    Added powersave and reset features

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -24,4 +24,29 @@ describe("Thermostat: ", function(){
       expect(thermostat.temp).toBe(10);
     });
   });
+  describe("#powerSave mode- ", function(){
+    it("is on by default", function(){
+      expect(thermostat.powerSave).toBe(true);
+    });
+    it("can be toggled off", function(){
+      thermostat.powerSaveSet(false);
+      expect(thermostat.powerSave).toBe(false);
+    });
+    it("caps maximum temperature at 25 in power save mode", function(){
+      thermostat.increase(6);
+      expect(thermostat.temp).toBe(25);
+    });
+    it("caps maximum temperature to 32 when power save mode is off", function(){
+      thermostat.powerSaveSet(false);
+      thermostat.increase(13);
+      expect(thermostat.temp).toBe(32);
+    });
+  });
+  describe("#reset-", function(){
+    it("resets temperature to 20", function(){
+      thermostat.increase(4);
+      thermostat.reset();
+      expect(thermostat.temp).toBe(20);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,15 +2,33 @@
 
 var Thermostat = function() {
   this.temp = 20;
+  this.powerSave = true;
 };
 
-Thermostat.prototype.temp = function(temp){
-  return temp;
+Thermostat.prototype.temp = function(){
+  return this.temp;
+};
+
+Thermostat.prototype.powerSave = function(){
+  return this.powerSave
+};
+
+Thermostat.prototype.powerSaveSet = function(bool){
+   this.powerSave = bool;
 };
 
 Thermostat.prototype.increase = function(temp){
   this.temp += temp;
+  if (this.temp > 25 && this.powerSave === true){
+    this.temp = 25;
+  } else if (this.temp > 32 && this.powerSave === false){
+    this.temp = 32;
+  };
 };
+
+Thermostat.prototype.reset = function(){
+  this.temp = 20;
+}
 
 Thermostat.prototype.decrease = function(temp){
   this.temp -= temp;
@@ -18,12 +36,4 @@ Thermostat.prototype.decrease = function(temp){
   if(this.temp < 10){
     this.temp = 10;
   };
-
-  // if((this.temp -= temp) < 10){
-  //     console.log("Temperature2: " + this.temp)
-  //   return this.temp = 10
-  //
-  // } else {
-  //   return this.temp -= temp;
-  // };
 };
